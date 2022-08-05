30d364f08db038cc78a2a007974f7565d55cdeb8
Annu <annusinghraj1998@gmail.com>
2022-08-06 01:19:59 +0530

    CREATED: Created first spring app application

---
+++ src/main/java/com/bridgelabz/HelloWorldSpringAppApplication/HelloWorldSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.HelloWorldSpringAppApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloWorldSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/HelloWorldSpringAppApplication/HelloWorldSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.HelloWorldSpringAppApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
