1cb384cc53fd97627234c1a1371f6a949b968ccb
Annu <annusinghraj1998@gmail.com>
2022-08-06 01:56:57 +0530

    ADDED: Used get request method and pass name as query parameter

--- src/main/java/com/bridgelabz/HelloWorldSpringAppApplication/controller/HelloWorldSpringAppApplicationController.java
+++ src/main/java/com/bridgelabz/HelloWorldSpringAppApplication/controller/HelloWorldSpringAppApplicationController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.helloworldspringappapplication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,11 @@ public class HelloWorldSpringAppApplicationController {
     @GetMapping("getdata")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/query")
+    public String queryParam(@RequestParam String name) {
+        return "Hello " + name + " from Bridgelabz";
     }
 
 }
